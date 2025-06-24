195bfe3ce115b0d047718ae4201f4558e33618c4
pedrosiufc <pedrosiufc@gmail.com>
2025-06-24 11:53:02 -0300

    Criação da pasta services e refatorando o código.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
+import { user } from "/src/scripts/services/user.js";
+import { repositories } from "/src/scripts/services/repositories.js";
+
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value;
     getUserProfile(userName);
@@ -10,32 +13,6 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     getUserProfile(userName);
   }
 })
-
-async function user(userName){
-    const response = await fetch(`https://api.github.com/users/${userName}`)
-    return await response.json();
-}
-
-async function repos(userName){
-    const response = await fetch(`https://api.github.com/users/${userName}/repos`)
-    return await response.json();
-}
-
-// function getUserProfile() {
-//     user().then(data => {
-//         const profile = document.querySelector('.profile-data');
-//         profile.innerHTML = `
-//             <img src="${data.avatar_url}" alt="${data.name}">
-//             <h1>${data.name}</h1>
-//             <p>${data.bio}</p>            
-//         `;
-//     }).catch(error => { 
-//         console.error('Error fetching user data:', error);
-//         const profile = document.querySelector('.profile-data');
-//         profile.innerHTML = `<p>Error loading profile.</p>`;
-//     });
-// }
-
 
 function getUserProfile(userName){  
           
@@ -55,7 +32,7 @@ function getUserProfile(userName){
 
 
 function getUserRepositories(userName) {
- repos(userName).then(reposData => {
+ repositories(userName).then(reposData => {
         let repositoriesItens = "";
         reposData.forEach(repo => {
             
