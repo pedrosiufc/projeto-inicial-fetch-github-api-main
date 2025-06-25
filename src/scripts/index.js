1662df0c278241a532cf0f50bdb180497372b60a
pedrosiufc <pedrosiufc@gmail.com>
2025-06-24 21:28:31 -0300

    Criação do objeto screen para reinderização da página.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,49 +1,33 @@
-import { user } from "/src/scripts/services/user.js";
-import { repositories } from "/src/scripts/services/repositories.js";
+import { getUser } from "/src/scripts/services/user.js";
+import { getRepositories } from "/src/scripts/services/repositories.js";
+
+import {user} from "/src/scripts/objetcs/user.js";
+import { screen } from "/src/scripts/objetcs/screen.js";
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value;
-    getUserProfile(userName);
+    getUserData(userName);
 })
 
 document.getElementById('input-search').addEventListener('keyup', (e) => {
   const userName = e.target.value;
     // Verifica se a tecla pressionada é 'Enter' e se o campo de entrada não está vazio
   if (e.key === 'Enter' && userName) {  
-    getUserProfile(userName);
+    getUserData(userName);
   }
 })
 
-function getUserProfile(userName){  
+async function getUserData(userName){  
+
+    const userResponse = await getUser(userName)
+    const repositoriesResponse = await getRepositories(userName)
+    
+    user.setInfo(userResponse);
+    user.setRepositories(repositoriesResponse)
+    
+
+    screen.renderUser(user);
           
-    user(userName).then(userData => {
-        let userInfo = `<div class="info"><img src="${userData.avatar_url}" alt="Foto do perfil do usuário"/>
-                <div class="data">
-                    <h1>${userData.name ?? 'Não possui nome cadastrado '}</h1>
-                    <p>${userData.bio ?? 'Não possui bio cadastrada'}</p>
-                </div>
-            </div>`
-
-            document.querySelector('.profile-data').innerHTML = userInfo;
-         
-            getUserRepositories(userName);
-    })
- }
-
-
-function getUserRepositories(userName) {
- repositories(userName).then(reposData => {
-        let repositoriesItens = "";
-        reposData.forEach(repo => {
-            
-            repositoriesItens += `<li> <a href="${repo.html_url}" target="_blank"> ${repo.name}</a></li>`;
-        })
-        document.querySelector('.profile-data').innerHTML += 
-                                                            `<div class="repositories section">
-                                                                 <h2>Repositórios</h2>
-                                                                 <ul>${repositoriesItens}</ul>
-                                                            </div>`;
-   })
+    
 }
-
 
